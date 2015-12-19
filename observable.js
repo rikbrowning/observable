9da2f799796362f4dc8ee6c2211084755a1c2e4e
rikstar <rikyb88@gmail.com>
2015-12-19 21:38:17 +0000

    Adding unsubscribe

--- observable.js
+++ observable.js
@@ -3,6 +3,13 @@
 
   window.rk.observable = {
     create: function (val) {
+      //confirm we havent got a observable
+      var descriptor = Object.getOwnPropertyDescriptor(val, "value");
+      if (descriptor && descriptor.set && descriptor.get && val.subscribe && val.unsubscribe) {
+        return val;
+      }
+
+      //create a new observable      
       var _value = val;
       var callbacks = {};
       return {
@@ -19,6 +26,11 @@
           var key = Date.now().toString();
           callbacks[key] = cb;
           return key;
+        },
+        unsubscribe: function (key) {
+          if (callbacks[key]) {
+            delete callbacks[key]
+          }
         }
       }
     }
